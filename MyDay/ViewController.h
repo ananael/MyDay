cb4a576274d953a91aab898f3f200ba44457eae4
Michael Hoffman <mhoffman999@gmail.com>
2016-02-26 16:33:51 -0500

    First commit, includes Run Script for auto-increment to build number.

---
+++ MyDay/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyDay
+//
+//  Created by Michael Hoffman on 2/26/16.
+//  Copyright © 2016 Here We Go. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
